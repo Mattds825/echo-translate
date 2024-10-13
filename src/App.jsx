4c18bf7a39aaa913b0c6438b980f5d0e19c14b2b
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-10-13 11:19:04 +0100

    add FileDisplay component with reset button functinoality passed down form App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+
+  const isAudioAvailable = file || audioStream;
+
+  const handleAudioReset = () => {
+    setFile(null);
+    setAudioStream(null);
+  };
+
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        <HomePage />
+        {/* Render Home Page if no audio is available yet, otherwise show FileDisplay */}
+        {isAudioAvailable ? (
+          <FileDisplay
+            file={file}
+            audioStream={audioStream}
+            handleAudioReset={handleAudioReset}
+          />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
         <footer></footer>
       </section>
     </div>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const { setFile, setAudioStream } = props;
   return (
     <main
       className="flex-1 p-4 flex flex-col gap-3
@@ -13,20 +14,34 @@ const HomePage = () => {
         Record <span className="text-indigo-400">&rarr;</span> Transcribe{" "}
         <span className="text-indigo-400">&rarr;</span> Translate{" "}
       </h3>
-      <button className="flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 px-4 py-2 rounded-xl specialBtn">
+      <button className="flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 px-3 py-2 rounded-xl specialBtn">
         <p className="text-indigo-400">Record</p>
         <i className="fa-solid fa-microphone"></i>
       </button>
       <p className="text-base">
-        Or <label
+        Or{" "}
+        <label
           className="text-indigo-400 cursor-pointer hover:text-blue-600 duration-200"
           htmlFor="fileInput"
         >
-          upload <input id="fileInput" className="hidden" type="file" accept=".mp3, .wave" />
+          upload{" "}
+          <input
+          onChange={(e) => {
+            const tempFile = e.target.files[0];
+            setFile(tempFile);
+          }}
+            id="fileInput"
+            className="hidden"
+            type="file"
+            accept=".mp3, .wave"
+          />
         </label>
         an audio file
       </p>
-      <p className="italic text-slate-500">Free now & free <span className="underline decoration-indigo-400">forever</span></p>
+      <p className="italic text-slate-500">
+        Free now & free{" "}
+        <span className="underline decoration-indigo-400">forever</span>
+      </p>
     </main>
   );
 };
