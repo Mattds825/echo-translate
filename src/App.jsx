5f5f9e3af3d3e0798d8b62f7295e3c1856b40884
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-10-13 10:50:05 +0100

    create HomePage and Header components, add basic layout and some stylling for HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <div>
-      App
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        <HomePage />
+        <footer></footer>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
