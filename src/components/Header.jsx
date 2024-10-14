e242494bfd6c6ac16a423f1356386c12fd0868e1
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-10-14 10:11:14 +0100

    add content of Informatin components with condiational redering of Transcripion or Translation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
+      <a href="/">
       <h1 className="font-medium">
         Echo<span className="text-indigo-400 bold">Translate</span>
-      </h1>
-      <button className="flex items-center gap-2 px-3 py-2 rounded-lg text-sm text-indigo-400 specialBtn">
+      </h1></a>
+      <a href="/" className="flex items-center gap-2 px-3 py-2 rounded-lg text-sm text-indigo-400 specialBtn">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
-      </button>
+      </a>
     </header>
   );
 };
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Transcription from "./Transcription";
+import Translation from "./Translation";
 
 const Information = () => {
+  const [tab, setTab] = useState("transcription");
   return (
-    <div>Information</div>
-  )
-}
+    <main
+      className="flex-1 p-4 flex flex-col gap-3
+  sm:gap-4 justify-center text-center pb-20 max-w-prose w-full mx-auto"
+    >
+      <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-normal">
+        Your <span className="text-indigo-400 bold">Transcription</span>
+      </h1>
 
-export default Information
+      <div className="grid grid-cols-2 items-center border-2 border-solid border-indigo-400 shadow rounded-full overflow-hidden mx-auto">
+        <button onClick={() => setTab("transcription")} className={"px-4 py-1 duration-200 font-medium " + (tab === "transcription" ? "bg-indigo-400 text-white" : "text-indigo-400 hover:text-indigo-600")}>Transcription</button>
+        <button onClick={() => setTab("translation")} className={"px-4 py-1 duration-200 font-medium " + (tab === "translation" ? "bg-indigo-400 text-white" : "text-indigo-400 hover:text-indigo-600")}>Translation</button>
+      </div>
+      {tab === "transcription" ? (<Transcription />) : (<Translation />)}
+    </main>
+  );
+};
+
+export default Information;
